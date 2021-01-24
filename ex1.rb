42a53fd8b0841f97c716508e5f9262cce55201d1
Raji0104 <arajarajeswari27@gmail.com>
2021-01-24 20:07:42 +0530

    first commit

---
+++ ex1.rb
@@ -0,0 +1,10 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yah! Printing."
+puts "I'd rather much you 'not'."
+puts 'I "said" do not touch this.'
+puts "Hi,\n How are you,\n I am Fine"
+print "Hi, How are you, I am Fine"
+#The First EXercise is over
